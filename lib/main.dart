02a210502781d3e6d0d2d60ba30aefb1700c0761
Berkayuzel <48026390+Berkayuzel@users.noreply.github.com>
2019-10-14 18:05:02 +0300

    14 ekim 2019

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,31 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child:FlatButton(
+              onPressed: (){
+                print('Left Button on Pressed.');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child:FlatButton(
+              onPressed: (){
+                print('Right Button on Pressed.');
+              },
+              child: Image.asset(
+                'images/dice2.png'
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+
   }
 }
